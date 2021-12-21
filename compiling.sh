b6e091592a97a17489d81be78de4e667c8e2ea17
TintedFireglass <tintedfireglass@hotmail.com>
2021-12-21 16:24:19 +0530

    Update compiling.sh

--- compiling.sh
+++ compiling.sh
@@ -1,9 +1,5 @@
 apt update && apt upgrade
-#separated them as it is more readable
-apt install git
-apt install nano 
-apt install proot
-apt install cmake 
+apt install git nano proot cmake 
 git clone https://github.com/moneroocean/xmrig.git
 cd xmrig && mkdir build && cd build
 cmake -DWITH_HWLOC=OFF ..
